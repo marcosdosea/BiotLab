5a56c857c6f3724c7b31f86d1d0e4cf256cfccbe
XIanTavares <iantavares.se@outlook.com>
2024-09-27 23:43:29 -0300

    Implementação do caso de uso Manter Gaiola closed #70

---
+++ Codigo/BiotLabWeb/BiotLabWeb/Models/GaiolaViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace BiotLabWeb.Models
+{
+    public class GaiolaViewModel
+    {
+        public int Id { get; set; }
+
+        public int CodigoGaiola { get; set; }
+
+        public int NumeroRatos { get; set; }
+
+        public string Localizacao { get; set; } = null!;
+
+        public int BioterioId { get; set; }
+
+        public int? ExperimentoId { get; set; }
+
+        public string Status { get; set; } = null!;
+    }
+}
---
+++ Codigo/BiotLabWeb/Service/GaiolaService.cs
@@ -0,0 +1,49 @@
+﻿using core;
+using core.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service
+{
+    public class GaiolaService : IGaiolaService
+    {
+        private readonly BiotLabContext context;
+
+        public GaiolaService(BiotLabContext context)
+        {
+            this.context = context;
+        }
+
+        public int Create(Gaiola gaiola)
+        {
+            context.Add(gaiola);
+            context.SaveChanges();
+            return gaiola.Id;
+        }
+
+        public void Delete(int id)
+        {
+            var gaiola = context.Gaiolas.Find(id);
+            if (gaiola != null)
+            {
+                context.Remove(gaiola);
+                context.SaveChanges();
+            }
+        }
+
+        public Gaiola Get(int id)
+        {
+            return context.Gaiolas.Find(id);
+        }
+
+        public IEnumerable<Gaiola> GetAll()
+        {
+            return context.Gaiolas.AsNoTracking();
+        }
+
+        public void Update(Gaiola gaiola)
+        {
+            context.Update(gaiola);
+            context.SaveChanges();
+        }
+    }
+}

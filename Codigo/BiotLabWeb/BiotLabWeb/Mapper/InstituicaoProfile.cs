bbf5ec25e7c7fba927f464b503ddf5d5559502b7
IagoLResende <Liberato_resende@hotmail.com>
2024-09-30 21:55:15 -0300

    Adicionado o caso de uso manter instituicao

---
+++ Codigo/BiotLabWeb/BiotLabWeb/Mapper/InstituicaoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BiotLabWeb.Models;
+using core;
+
+namespace BiotLabWeb.Mapper
+{
+    public class InstituicaoProfile : Profile
+    {
+        public InstituicaoProfile()
+        {
+            CreateMap<Instituicao, InstituicaoViewModel>().ReverseMap();
+        }
+    }
+}
---
+++ Codigo/BiotLabWeb/BiotLabWeb/Models/InstituicaoViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace BiotLabWeb.Models
+{
+    public class InstituicaoViewModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; } = null!;
+
+        public string Cnpj { get; set; } = null!;
+
+        public string Estado { get; set; } = null!;
+
+        public string? Telefone { get; set; }
+
+        public string Email { get; set; } = null!;
+    }
+}

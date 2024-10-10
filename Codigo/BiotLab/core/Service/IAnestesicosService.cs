112839216867274aff2f46ece24c80cad19cfaba
XIanTavares <iantavares.se@outlook.com>
2024-10-10 10:32:09 -0300

    Implementada a service de ManterAnestesico close #87

---
+++ Codigo/BiotLab/core/Service/IAnestesicosService.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IAnestesicosService
+    {
+
+        public void Update(Anestesico anestesico);
+        public void Delete(uint id);
+        public bool Validar(uint id);
+        public Anestesico? Buscar(uint id);
+    }
+}

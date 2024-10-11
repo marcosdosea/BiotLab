06c6620e69fa8e8f65393a8dcb379cc95ef5b174
IagoLResende <Liberato_resende@hotmail.com>
2024-10-11 20:16:35 -0300

    Implementado o InstituicaoDTO outros arquivos referente ao service já estão na main

---
+++ Codigo/BiotLab/core/DTO/InstituicaoDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.DTO
+{
+    internal class InstituicaoDTO
+    {
+        public uint Id { get; set; }
+
+        public required string Nome { get; set; }
+
+        public required string Cnpj { get; set; }
+
+        public required string Cep { get; set; }
+
+        public required string Estado { get; set; }
+    }
+}

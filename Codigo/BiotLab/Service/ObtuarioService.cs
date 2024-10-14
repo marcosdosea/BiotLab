5c7c0b21f851513b200bfbf5b86224b08e020c1e
XIanTavares <iantavares.se@outlook.com>
2024-10-14 19:53:33 -0300

    Implementação da service "Manter Obituário" , resolve #89

---
+++ Codigo/BiotLab/Service/ObtuarioService.cs
@@ -0,0 +1,42 @@
+﻿using Core.Service;
+using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class ObituarioService : IObituarioService
+    {
+        private readonly BiotlabContext context;
+
+        public ObituarioService(BiotlabContext context)
+        {
+            this.context = context;
+        }
+
+        public Obituario? Buscar(uint id)
+        {
+            return context.Obituarios.Find(id);
+        }
+
+        public uint Create(Obituario obituario)
+        {
+            context.Add(obituario);
+            context.SaveChanges();
+            return obituario.Id;
+        }
+
+        public void Delete(uint id)
+        {
+            var obituario = context.Obituarios.Find(id);
+            if (obituario != null)
+            {
+                context.Obituarios.Remove(obituario);
+                context.SaveChanges();
+            }
+        }
+    }
+}

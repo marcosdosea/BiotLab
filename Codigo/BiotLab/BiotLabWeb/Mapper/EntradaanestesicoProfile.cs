4764f9b651d583fddf49a16a38931302c7c291f3
Felip3D3V <143296850+Felip3D3V@users.noreply.github.com>
2024-10-15 22:59:59 -0300

    adicionado a controller e o mapper #closed 118

---
+++ Codigo/BiotLab/BiotLabWeb/Mapper/EntradaanestesicoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BiotLabWeb.Models;
+using Core;
+
+namespace BiotLabWeb.Mapper
+{
+    public class EntradaanestesicoProfile : Profile
+    {
+        public EntradaanestesicoProfile()
+        {
+            CreateMap<Entradaanestesico, EntradaanestesicoViewModel>()
+               
+        }
+    }
+}

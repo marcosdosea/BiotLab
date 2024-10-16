302884f53badfedee36fbc5766d9e2c614e14460
NadsonTelesMendonca <nadsonteles@hotmail.com>
2024-10-15 23:51:21 -0300

    Adicionado Teste Service De experimento.

---
+++ Codigo/BiotLab/core/DTO/ExperimentoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.DTO
+{
+    internal class ExperimentoDTO
+    {
+        public uint Id { get; set; }
+
+        public string Nome { get; set; } = null!;
+
+        public string DataInicio { get; set; } = null!;
+
+        public string DataFim { get; set; } = null!;
+    }
+}

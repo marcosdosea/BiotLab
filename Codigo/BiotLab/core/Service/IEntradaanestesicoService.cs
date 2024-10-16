dbf8ae5796b35107410b607ef5b602565769d9fc
Felip3D3V <143296850+Felip3D3V@users.noreply.github.com>
2024-10-15 22:46:57 -0300

    Foi adicionado IEntradaanestesicoService.cs e EntradaanestesicoService.cs #closed

---
+++ Codigo/BiotLab/core/Service/IEntradaanestesicoService.cs
@@ -0,0 +1,14 @@
+﻿using Core;
+using System.Collections.Generic;
+
+namespace Core.Service
+{
+    public interface IEntradaanestesicoService
+    {
+        uint Create(Entradaanestesico entradaAnestesico);
+        void Update(Entradaanestesico entradaAnestesico);
+        void Delete(uint idEntrada, uint idAnestesico);
+        IEnumerable<Entradaanestesico> GetAll();
+        Entradaanestesico Get(uint idEntrada, uint idAnestesico);
+    }
+}

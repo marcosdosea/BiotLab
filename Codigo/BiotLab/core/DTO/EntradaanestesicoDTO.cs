2f7580f5b8bdde786ee4e853dfcf5c53eef9f2a0
Felip3D3V <143296850+Felip3D3V@users.noreply.github.com>
2024-10-22 19:39:25 -0300

    Adicionado DTO de EntradaanestesicoDTO.cs

---
+++ Codigo/BiotLab/core/DTO/EntradaanestesicoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Core.DTO
+{
+    internal class EntradaanestesicoDTO
+    {
+        public uint IdEntrada { get; set; }
+        public uint IdAnestesico { get; set; }
+        public decimal Quantidade { get; set; }
+        public string Lote { get; set; } = null!;
+        public decimal ValorUnitario { get; set; }
+        public decimal SubTotal { get; set; }
+    }
+}

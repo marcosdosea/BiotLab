c0b0df11167905673d460faa7828c7b2b805866d
NadsonTelesMendonca <nadsonteles@hotmail.com>
2024-10-25 17:04:48 -0300

    Adição de DTO e Program Obtuario.

---
+++ Codigo/BiotLab/core/DTO/ObituarioDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTO
+{
+    internal class ObituarioDTO
+    {
+        public uint Id { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public uint IdGaiola { get; set; }
+
+        public uint IdPesquisador { get; set; }
+    }
+}

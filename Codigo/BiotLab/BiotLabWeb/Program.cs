b6658e1a0b5aeb125bbdb3a0e3bc9ee58b4c468f
XIanTavares <iantavares.se@outlook.com>
2024-11-04 12:50:29 -0300

    Implementada a autenticação com o identity framework seguindo as boas práticas, Modificada a Index e removido o formulário de login desnecessário, Configurada as opções de autenticação, Adicionado o botão de login na navbar. close #212

--- Codigo/BiotLab/BiotLabWeb/Program.cs
+++ Codigo/BiotLab/BiotLabWeb/Program.cs
@@ -2,6 +2,8 @@ using Core;
 using Core.Service;
 using Microsoft.EntityFrameworkCore;
 using Service;
+using BiotLabWeb.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace BiotLabWeb
 {
@@ -13,11 +15,6 @@ namespace BiotLabWeb
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
-            builder.Services.AddDbContext<BiotlabContext>(options =>
-            {
-                options.UseMySQL(builder.Configuration.GetConnectionString("BiotLabConnection"));
-            });
-
             builder.Services.AddTransient<IGaiolaService, GaiolaService>();
             builder.Services.AddTransient<IInstituicaoService, InstituicaoService>();
             builder.Services.AddTransient<IHaremService, HaremService>();
@@ -28,6 +25,36 @@ namespace BiotLabWeb
             builder.Services.AddTransient<IGaiolaharemService, GaiolaharemService>();
             builder.Services.AddTransient<IUsoanestesicoService, UsoanestesicoService>();
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            builder.Services.AddDbContext<BiotlabContext>(options => options.UseMySQL(builder.Configuration.GetConnectionString("BiotLabConnection")));
+            builder.Services.AddDbContext<IdentityContext>(options => options.UseMySQL(builder.Configuration.GetConnectionString("IdentityConnection")));
+
+            builder.Services.AddDefaultIdentity<UsuarioIdentity>(options =>
+            {
+                // SignIn settings
+                options.SignIn.RequireConfirmedAccount = true;
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+
+                // Default User settings.
+                options.User.AllowedUserNameCharacters =
+                        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+
+                // Default Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+            }).AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<IdentityContext>();
+
             var app = builder.Build();
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -42,7 +69,10 @@ namespace BiotLabWeb
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
+
+            app.MapRazorPages();
 
             app.MapControllerRoute(
                 name: "default",

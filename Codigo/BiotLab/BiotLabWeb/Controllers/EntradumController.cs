8fba44bbf53856463fe362130ea914643cda0582
NadsonTelesMendonca <nadsonteles@hotmail.com>
2024-11-04 13:50:26 -0300

    criar partes restantes e corrigir erros partes do CSU Entradum.

---
+++ Codigo/BiotLab/BiotLabWeb/Controllers/EntradumController.cs
@@ -0,0 +1,101 @@
+﻿using AutoMapper;
+using BiotLabWeb.Models; 
+using Core;
+using Core.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BiotLabWeb.Controllers
+{
+    public class EntradumController : Controller
+    {
+        private readonly IEntradumService entradumService; 
+        private readonly IMapper mapper;
+
+        public EntradumController(IEntradumService entradumService, IMapper mapper)
+        {
+            this.entradumService = entradumService;
+            this.mapper = mapper;
+        }
+
+        public ActionResult Index()
+        {
+            var entradas = entradumService.GetAll(); 
+            var entradaViewModels = mapper.Map<List<EntradumViewModel>>(entradas); 
+            return View(entradaViewModels);
+        }
+
+      
+        public ActionResult Details(uint id)
+        {
+            var entradum = entradumService.Get(id);
+            if (entradum == null)
+            {
+                return NotFound();
+            }
+            var entradumViewModel = mapper.Map<EntradumViewModel>(entradum);
+            return View(entradumViewModel);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EntradumViewModel entradum)
+        {
+            if (ModelState.IsValid)
+            {
+                var entradumEntity = mapper.Map<Entradum>(entradum);
+                entradumService.Create(entradumEntity);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(entradum);
+        }
+
+        public ActionResult Edit(uint id)
+        {
+            var entradum = entradumService.Get(id);
+            if (entradum == null)
+            {
+                return NotFound();
+            }
+            var entradumViewModel = mapper.Map<EntradumViewModel>(entradum);
+            return View(entradumViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(uint id, EntradumViewModel entradum)
+        {
+            if (ModelState.IsValid)
+            {
+                var entradumEntity = mapper.Map<Entradum>(entradum);
+                entradumService.Update(entradumEntity);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(entradum);
+        }
+
+        public ActionResult Delete(uint id)
+        {
+            var entradum = entradumService.Get(id);
+            if (entradum == null)
+            {
+                return NotFound();
+            }
+            var entradumViewModel = mapper.Map<EntradumViewModel>(entradum);
+            return View(entradumViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(uint id, EntradumViewModel entradum)
+        {
+            entradumService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Codigo/BiotLab/BiotLabWeb/Models/EntradumViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BiotLabWeb.Models
+{
+    public class EntradumViewModel
+    {
+        [Display(Name = "Código do Registro")]
+        [Required(ErrorMessage = "O código do registro é obrigatório")]
+        [Key]
+        public uint Id { get; set; }
+
+        [Display(Name = "Número da Nota Fiscal")]
+        [Required(ErrorMessage = "O número da nota fiscal é obrigatório")]
+        [StringLength(50, ErrorMessage = "O número deve ter no máximo 50 caracteres")]
+        public string NumeroNotaFiscal { get; set; } = string.Empty; // Número da Nota Fiscal
+
+        [Display(Name = "Data de Entrada")]
+        [Required(ErrorMessage = "A data de entrada é obrigatória")]
+        public DateTime DataEntrada { get; set; } // Data de entrada
+
+        [Display(Name = "Código do Fornecedor")]
+        [Required(ErrorMessage = "O código do fornecedor é obrigatório")]
+        public uint IdFornecedor { get; set; } // Identificador do fornecedor
+
+        public FornecedorViewModel Fornecedor { get; set; } = new FornecedorViewModel(); // Representação do fornecedor
+
+        [Display(Name = "Código da Instituição")]
+        [Required(ErrorMessage = "O código da instituição é obrigatório")]
+        public uint IdInstituicao { get; set; } // Identificador da instituição
+
+        public InstituicaoViewModel Instituicao { get; set; } = new InstituicaoViewModel(); // Representação da instituição
+
+    }
+}
